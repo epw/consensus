eeed7194dccef40bde24d0cb898c2787b483864f
Eric Willisson <ericwillisson@gmail.com>
2015-07-26 23:18:00 -0400

    Adjusted some positioning

--- render-all.sh
+++ render-all.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-playbooks="cabalist.tex guru.tex hedge_mage.tex pious.tex primordial.tex tech_adept.tex"
+playbooks="cabalist.tex guru.tex hedge_mage.tex inspired.tex pious.tex primordial.tex tech_adept.tex voiced.tex wayfarer.tex"
 
 for playbook in $playbooks
 do
