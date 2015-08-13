b505396cd7f75eb10fbc420f3c619987fbba5877
Eric Willisson <ericwillisson@gmail.com>
2015-08-12 23:04:38 -0400

    Replaced render-all.sh with Makefile

--- render-all.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-playbooks="cabalist.tex guru.tex hedge_mage.tex inspired.tex pious.tex primordial.tex tech_adept.tex voiced.tex wayfarer.tex"
-
-for playbook in $playbooks
-do
-    xelatex $playbook
-done
