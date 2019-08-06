dfd2bab50995844e092f734074163bc680ac989f
Jacob <themobpenguin2@gmail.com>
2019-08-06 10:50:21 -0700

    fixed links in html

--- pageref.lua
+++ pageref.lua
@@ -2,7 +2,9 @@ local function pagereflink(format, link, before, after)
   if format:match 'latex' then
     return pandoc.RawInline('latex', before .. link .. ' (page \\pageref{' .. link .. '})' .. after)
   elseif format:match 'html.*' then
-    return pandoc.RawInline('html',before .. '<a href=#' .. link .. '>' .. link .. '</a>' .. after)
+    link_href = string.lower(link)
+    link_href = string.gsub(link_href, "%s+", "_")
+    return pandoc.RawInline('html',before .. '<a href="#' .. link_href .. '">' .. link .. '</a>' .. after)
   else
     return pandoc.Str("Whoops")
   end
@@ -12,7 +14,9 @@ local function pagerefanchor(format, anchor, before, after)
   if format:match 'latex' then
     return pandoc.RawInline('latex', before ..'\\label{' .. anchor .. '}' .. after)
   elseif format:match 'html.*' then
-    return pandoc.RawInline('html', before .. '<a id="' .. anchor .. '"></a>' .. after)
+    anchor_id = string.lower(anchor)
+    anchor_id = string.gsub(anchor_id, "%s+", "_")
+    return pandoc.RawInline('html', before .. '<a id="' .. anchor_id .. '"></a>' .. after)
   else
     return pandoc.Str("Whoops")
   end
